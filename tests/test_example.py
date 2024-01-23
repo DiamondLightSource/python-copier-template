431c71e638b5e8a4600d1e8cc2659337a978dc6e
Tom Cobb <tom.cobb@diamond.ac.uk>
2024-01-23 17:24:06 +0000

    Added tests

---
+++ tests/test_example.py
@@ -0,0 +1,24 @@
+from copier import run_copy
+from pathlib import Path
+import treecomp
+import yaml
+import shutil
+
+TOP = Path(__file__).absolute().parent.parent
+INPUT = TOP / "python-copier-template-example"
+OUTPUT = TOP / "build" / "example"
+
+
+def test_template():
+    shutil.rmtree(OUTPUT, ignore_errors=True)
+    answers = yaml.safe_load((INPUT / ".copier-answers.yml").read_text())
+    run_copy(
+        src_path=str(TOP),
+        dst_path=OUTPUT,
+        data=answers,
+        vcs_ref="HEAD",
+        unsafe=True,
+    )
+    comparison = treecomp.diff_file_trees(INPUT, OUTPUT, ignore=[".copier-answers.yml"])
+    if comparison.diffs:
+        raise AssertionError(str(comparison))

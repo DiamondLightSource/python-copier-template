67a2937511d5d6c3b9ed4f5ca9f94b602b9bbc9e
Gary Yendell <gary.yendell@diamond.ac.uk>
2024-02-29 16:02:45 +0000

    Add pytest configuration for break on exception
    Pytest nicely catches raised exceptions during tests and reports them
    at the end. This is very useful except when debugging tests in vscode
    because it prevents the debugger from pausing when there is an exception
    and displaying the call stack.
    
    This adds hooks for pytest to re-raise the caught exceptions when an
    environment variable is set, and then sets that environment variable
    in the `Debug Unit Test` launch configuration.

---
+++ tests/conftest.py
@@ -0,0 +1,15 @@
+import os
+
+import pytest
+
+# Prevent pytest from catching exceptions when debugging in vscode so that break on
+# exception works correctly (see: https://github.com/pytest-dev/pytest/issues/7409)
+if os.getenv("PYTEST_RAISE", "0") == "1":
+
+    @pytest.hookimpl(tryfirst=True)
+    def pytest_exception_interact(call):
+        raise call.excinfo.value
+
+    @pytest.hookimpl(tryfirst=True)
+    def pytest_internalerror(excinfo):
+        raise excinfo.value
